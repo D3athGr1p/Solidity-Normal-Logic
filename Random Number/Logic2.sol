21c566df6b43768e6d92d58cb83c815827692d38
D3athGr1p <raininfotech14@gmail.com>
2022-06-28 13:20:44 +0530

    buyWethUsingUniswap | Check_Multiple_Token_Balance_Single_RPC_Call | ChainLink

--- Random Number/Logic2.sol
+++ Random Number/Logic2.sol
@@ -1,14 +1,28 @@
+// "SPDX-License-Identifier: UNLICENSED"
+
 pragma solidity ^0.8.0;
 
 contract test {
-    uint[] num;
+    uint256[] num;
+
     function addArray() public {
         num.push(1);
     }
-    function getHash() internal returns(uint256){
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, num.length)));
+
+    function getHash() internal view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.difficulty,
+                        block.timestamp,
+                        num.length
+                    )
+                )
+            );
     }
-    function getRandomNumber() public returns(uint256, uint256) {
+
+    function getRandomNumber() public view returns (uint256, uint256) {
         return (num.length, getHash() % num.length);
     }
 }
